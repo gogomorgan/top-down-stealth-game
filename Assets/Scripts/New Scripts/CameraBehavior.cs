2a74808dfc73dc21939f616ee8c935e00aeb3f8d
GordonCoffeeCan <664220842@qq.com>
2016-11-30 17:44:33 -0500

    Gordon created something
    Add a new scene for testing character control. Use spotlight to light up
    some area as field of view. Shift to run, alt to sneak. Welcome to test.
    I'm looking forward to getting your feedback.

---
+++ Assets/Scripts/New Scripts/CameraBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour {
+    private Transform _trans;
+    private Transform _target;
+    private float _camSpeed = 3;
+    private float _camHeight;
+
+    private float deltaStep;
+
+	// Use this for initialization
+	void Start () {
+        _trans = this.transform;
+        _target = GameObject.Find("CameraCenter").transform;
+        _camHeight = _trans.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void FixedUpdate() {
+        deltaStep = _camSpeed;
+        _trans.position = Vector3.MoveTowards(_trans.position, _target.position, deltaStep);
+        _trans.position = new Vector3(_trans.position.x, _camHeight, _trans.position.z);
+    }
+}
---
+++ Assets/Scripts/New Scripts/PlayerControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float sneakSpeed = 3;
+    public float walkSpeed = 5;
+    public float runSpeed = 10;
+
+    public KeyCode sneak;
+    public KeyCode run;
+
+    private float moveSpeed;
+
+    private Vector3 _mosePosition;
+    private Transform _trans;
+    private CharacterController _playerController;
+
+	// Use this for initialization
+	void Start () {
+        _trans = this.transform;
+        _playerController = this.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate() {
+        _mosePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y));
+        _trans.LookAt(_mosePosition + Vector3.up * _trans.position.y);
+
+        if (Input.GetKey(sneak)) {
+            Debug.Log("sneak");
+            moveSpeed = sneakSpeed;
+        } else if (Input.GetKey(run)) {
+            Debug.Log("run");
+            moveSpeed = runSpeed;
+        }else{
+            moveSpeed = walkSpeed;
+        }
+
+        _playerController.SimpleMove(new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") * moveSpeed));
+    }
+}
