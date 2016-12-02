26899a112e68d9aa7960650c7633082cb0d459b8
GordonCoffeeCan <gordonlee@172-16-15-73.DYNAPOOL.NYU.EDU>
2016-12-02 17:24:09 -0500

    Added spawner path and it can move itself
    add 3 path for 3 spawners. there are 8 spawners in the scene. I’m gonna
    add the rest later today. Maze is added T-shape wall that Rob mentioned
    today. small stuff fixed

---
+++ Assets/Scripts/New Scripts/SpawnerBehavior.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerBehavior : MonoBehaviour {
+	public float idleSpeed = 5;
+	public bool isPathReversed = false;
+	public string SpawnerPath = null;
+
+	private Transform _trans;
+
+	// Use this for initialization
+	void Start () {
+		_trans = this.transform;
+		if (SpawnerPath != null) {
+			moveOnPath ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void moveOnPath(){
+		iTween.MoveTo (_trans.gameObject, iTween.Hash ("path", iTweenPath.GetPath (SpawnerPath),"orienttopath", true, "speed", idleSpeed, "easetype", iTween.EaseType.linear, "oncomplete", "moveBackOnPath"));
+	}
+
+	private void moveBackOnPath(){
+		if (isPathReversed == true) {
+			iTween.MoveTo (_trans.gameObject, iTween.Hash ("path", iTweenPath.GetPathReversed (SpawnerPath), "orienttopath", true, "speed", idleSpeed, "easetype", iTween.EaseType.linear, "oncomplete", "moveOnPath"));
+		} else {
+			moveOnPath ();
+		}
+	}
+}
