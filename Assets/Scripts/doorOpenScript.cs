0548d716315126dff5e3a6ca96e70d5c3bce084e
Robert Rappoport <rjr363@nyu.edu>
2016-12-03 22:54:58 -0500

    actually changed stuff to door and key scripts

---
+++ Assets/Scripts/doorOpenScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorOpenScript : MonoBehaviour {
+
+	private Animator doorAnimator;
+	public AudioClip doorAudio;
+	AudioSource audio;
+	GameObject keyManager = GameObject.Find("keyChain");
+
+	// Use this for initialization
+	void Start () 
+	{
+		doorAnimator = this.GetComponent<Animator> ();
+		keyScript = keyManager.GetComponent<keyScript> ();
+		audio = GetComponent<AudioSource>();
+
+	
+	}
+
+	void onCollisionEnter (Collision col)
+	{
+		switch (col.gameObject.tag)
+		{
+		case "door1":
+			if(keyScript.key1Collected == true)
+			{
+				doorAnimator.SetBool ("Open", true);
+				audio.PlayOneShot(doorAudio, 1f);
+			}
+			break;
+
+		case "door2":
+			if (keyScript == true) 
+			{
+				doorAnimator.SetBool ("Open", true);
+				audio.PlayOneShot(doorAudio, 1f);
+			}
+			break;
+
+		case "door3":
+			if (keyScript == true) 
+			{
+				doorAnimator.SetBool ("Open", true);
+				audio.PlayOneShot(doorAudio, 1f);
+			}
+			break;
+
+		case "door4":
+			if (keyScript == true) 
+			{
+				doorAnimator.SetBool ("Open", true);
+				audio.PlayOneShot(doorAudio, 1f);
+			}
+			break;
+
+		
+		}
+	}
+}
---
+++ Assets/Scripts/keyScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class keyScript : MonoBehaviour {
+	public AudioClip keyAudio;
+	AudioSource audio;
+	public bool key1Collected = false;
+	public bool key2Collected = false;
+	public bool key3Collected = false;
+	public bool key4Collected = false;
+	public bool key5Collected = false;
+
+
+	void Start ()
+	{
+		audio = GetComponent<AudioSource>();
+	}
+
+	void onCollisionEnter (Collision col)
+	{
+		switch (col.gameObject.tag)
+		{
+		case "key1":
+			
+			audio.PlayOneShot(keyAudio, 1f);
+				key1Collected = true;
+			break;
+
+		case "key2":
+
+			audio.PlayOneShot(keyAudio, 1f);
+				key2Collected = true;
+			break;
+
+		case "key3":
+
+			audio.PlayOneShot(keyAudio, 1f);
+				key3Collected = true;
+			break;
+
+		case "key4":
+			
+			audio.PlayOneShot(keyAudio, 1f);
+				key4Collected = true;
+			break;
+
+		case "key5":
+			
+			audio.PlayOneShot(keyAudio, 1f);
+				key5Collected = true;
+			break;
+
+
+		}
+	}
+}
